23b00d7df12667b16e656ab102a9bfdeaf8be8f5
Jeremy <jdwurbs@gmail.com>
2023-09-29 00:56:14 +0900

    Remove reference to TextToImage class

--- dosaku/modules/__init__.py
+++ dosaku/modules/__init__.py
@@ -1,4 +1,3 @@
-from dosaku.modules.huggingface.text_to_image import TextToImage
 from dosaku.modules.json_serializer import JsonSerializer
 from dosaku.modules.xml_serializer import XmlSerializer
 from dosaku.modules.yaml_serializer import YamlSerializer
