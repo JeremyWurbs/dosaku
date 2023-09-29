98f4ced5b10cf8f7bfef1028a53b96c542878585
Jeremy <jdwurbs@gmail.com>
2023-09-29 14:13:39 +0900

    Update config documentation

--- dosaku/config/config.py
+++ dosaku/config/config.py
@@ -4,6 +4,34 @@ import os
 
 
 class Config:
+    """Dosaku Config.
+
+    To use the config parser, fill out the *config.ini* file. It includes paths to local directories as well as API keys
+    necessary to use some services, such as OpenAI (ChatGPT) or Clipdrop (Stable Diffusion).
+
+    Some notes to help you:
+        1. API keys are required by most services. Refer to the services documentation for more information.
+        2. You may use tildes (~) to denote the user home directory in the config file.
+        3. The config file may refer to other parts of itself using ${}. Refer to the config file itself for examples.
+        4. Most demos / scripts that ask for a resource path can be left blank if the config has the associated info.
+
+    Args:
+        config_path: The complete path to the .ini file. If not provided it will be looked for in the same directory as
+            this config.py file.
+
+    Example::
+
+        import json
+        from Dosaku import Config
+
+        config = Config()
+        print(config['DOSAKU']['ROOT_DIR'])  # '/home/user/dosaku'
+
+        config = Config().as_dict()  # May use Config.__str__() or Config.as_dict() for serializable operations
+        print(json.dumps(config, indent=4))
+
+    """
+
     def __init__(self, config_path: str = None):
         self.config = ConfigParser(interpolation=ExtendedInterpolation())
 
