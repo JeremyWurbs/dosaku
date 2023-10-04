ac4408160470f8c91cb05ecf08db50b0c3ec123c
Jeremy Wurbs <jeremy@formskorea.com>
2023-10-04 13:54:24 +0900

    Add Stable Diffusion Module class

--- dosaku/modules/__init__.py
+++ dosaku/modules/__init__.py
@@ -3,5 +3,6 @@ from dosaku.modules.samples.song_serializer.xml_serializer import XmlSerializer
 from dosaku.modules.samples.song_serializer.yaml_serializer import YamlSerializer
 from dosaku.modules.samples.chat.echo_bot import EchoBot
 from dosaku.modules.samples.chat.red_pajama import RedPajama
-from dosaku.modules.stability.text_to_image import ClipdropTextToImage
+from dosaku.modules.stability.clipdrop.text_to_image import ClipdropTextToImage
+from dosaku.modules.stability.sd_official import StableDiffusion
 from dosaku.modules.ultralytics.yolo8 import Yolo8
