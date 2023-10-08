fb8e3acdcca590e0b6e0de2d759990801c25467a
Jeremy <jdwurbs@gmail.com>
2023-10-08 16:58:37 +0900

    Move files

--- dosaku/tasks/__init__.py
+++ dosaku/tasks/__init__.py
@@ -2,6 +2,4 @@ from dosaku.tasks.song_serializer import SongSerializer
 from dosaku.tasks.text_to_image import TextToImage
 from dosaku.tasks.chat import Chat
 from dosaku.tasks.gradio_chat import GradioChat
-from dosaku.tasks.go.gtp_v1 import GoTextProtocolV1
-from dosaku.tasks.go.play_go import PlayGo
-from dosaku.tasks.go.go_gui import GoGUI
+from dosaku.tasks.gtp_v1 import GoTextProtocolV1
