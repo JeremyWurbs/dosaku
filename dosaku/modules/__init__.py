46acdebda7b3cf2a87057665d7e95f85382d2017
Jeremy <jdwurbs@gmail.com>
2023-10-08 16:53:23 +0900

    Update namespaces

--- dosaku/modules/__init__.py
+++ dosaku/modules/__init__.py
@@ -4,3 +4,5 @@ from dosaku.modules.samples.song_serializer.yaml_serializer import YamlSerialize
 from dosaku.modules.samples.chat.echo_bot import EchoBot
 from dosaku.modules.samples.chat.red_pajama import RedPajama
 from dosaku.modules.stability.text_to_image import ClipdropTextToImage
+from dosaku.modules.go.sabaki import Sabaki
+from dosaku.modules.go.random_go_bot import RandomGoBot
