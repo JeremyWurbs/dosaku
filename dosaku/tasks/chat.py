55df082470f64d2272c69d044c7e31794305a199
Jeremy <jdwurbs@gmail.com>
2023-10-16 22:00:16 +0900

    Update Chat task documentation

--- dosaku/tasks/chat.py
+++ dosaku/tasks/chat.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from typing import Generator, Union
 
 from dosaku import Task
 
@@ -11,24 +12,62 @@ class Chat(Task):
         super().__init__()
 
     @abstractmethod
-    def message(self, message: str, **kwargs):
+    def message(self, message: str, **kwargs) -> Union[str, Generator[str, None, None]]:
         """Send a message to the agent and get a response.
 
-        It is up to the chat module whether a chat history is kept and used.
+        It is up to the chat module whether a chat history is kept and used. It is also up to the module whether
+        streaming is supported.
 
         Args:
             message: The message to send the chat agent.
 
         Returns:
-            A response.
+            An AI chat response. The response may be in one of two forms:
 
-        Example::
+                - non-streaming: The expected default behavior, if a module is in non-streaming mode it should return
+                    the response as a string directly;
+                - streaming: If the module supports and is set to streaming, it should return a generator object that
+                    yields an updated string response every iteration;
+
+        Non-streaming Example::
 
             from dosaku import Agent
 
             agent = Agent()
             agent.learn('Chat', module='EchoBot')
-            response = agent.Chat.message('Hello!')  #
+            response = agent.Chat.message('Hello!')  # Hi, I'm EchoBot. You said: "Hello!".
+
+        Streaming Example::
+
+            from dosaku import Agent
+
+            agent = Agent()
+            agent.learn('Chat', module='EchoBot', stream=True)
+            for partial_response in agent.Chat.message('Hello!):
+                print(partial_response)
+
+            # H
+            # Hi
+            # Hi,
+            # ...
+            # Hi, I'm EchoBot. You said: "Hello
+            # Hi, I'm EchoBot. You said: "Hello!
+            # Hi, I'm EchoBot. You said: "Hello!"
+
+        OpenAI GPT Streaming in a Gradio App Example (Requires the OpenAI service)::
+
+            import gradio as gr
+            from dosaku import Agent, Config
+
+            agent = Agent(enable_services=True)
+            agent.learn('Chat', module='OpenAIChat', model=Config()['OPENAI']['DEFAULT_MODEL'], stream=True)
+
+            def predict(message, _):
+                for partial_response in agent.Chat(message):  # __call__() defaults to message()
+                    yield partial_response
+
+            gr.ChatInterface(predict).queue().launch()
+
         """
 
     @abstractmethod
