651e6f180ab2cc80343b758d2812498f47b117a0
Jeremy Wurbs <jdwurbs@gmail.com>
2023-10-24 03:15:41 +0900

    Add DeBERTa microsoft module

--- dosaku/modules/__init__.py
+++ dosaku/modules/__init__.py
@@ -4,6 +4,7 @@ from dosaku.modules.openai.chat import OpenAIChat
 from dosaku.modules.dosaku.coder import Coder
 from dosaku.modules.openai.spellchecker import Spellchecker
 from dosaku.modules.openai.whisper import Whisper
+from dosaku.modules.microsoft.deberta import DeBERTa
 from dosaku.modules.samples.song_serializer.json_serializer import JsonSerializer
 from dosaku.modules.samples.song_serializer.xml_serializer import XmlSerializer
 from dosaku.modules.samples.song_serializer.yaml_serializer import YamlSerializer
@@ -16,5 +17,6 @@ from dosaku.modules.stability.sd_official import StableDiffusion
 from dosaku.modules.stability.sdxl_eoe import SDXLEnsembleOfExperts
 from dosaku.modules.ultralytics.yolo8 import Yolo8
 from dosaku.modules.meta.sam import SegmentAnything
+from dosaku.modules.meta.bart import BART
 from dosaku.modules.tencent.gfpgan import GFPGAN
 from dosaku.modules.kakao.karlo import KarloImageBlend
