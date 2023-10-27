8af94c52163729763419afcb66f690d87e381988
Jeremy Wurbs <jdwurbs@gmail.com>
2023-10-27 16:25:01 +0900

    Add additional Dosaku exceptions

--- dosaku/core/exceptions.py
+++ dosaku/core/exceptions.py
@@ -1,3 +1,11 @@
+class ActionDoesNotExist(AttributeError):
+    """Raised when the current resource does not have the requested action attribute."""
+
+
+class CodingError(RuntimeError):
+    """Raised when generated code fails to pass an assertion or quality check."""
+
+
 class ExecutorPermissionRequired(PermissionError):
     """Raised when the current resource requires Executor permissions, but does not have it."""
 
@@ -8,6 +16,14 @@ class InterpreterError(Exception):
 
 class ModuleForTaskNotFound(ModuleNotFoundError):
     """Raised when trying to instantiate a Task but no suitable Module is found."""
+
+
+class NameAttributeNotFound(NotImplementedError):
+    """Raised when a resource does not have a name attribute."""
+
+
+class OptionNotSupported(NotImplementedError):
+    """Raised when an option is passed to a resource that it does not support."""
 
 
 class ServicePermissionRequired(PermissionError):
