7bc9a52bf8b282c42921a54b2ab156618b2bde32
Jeremy Wurbs <jdwurbs@gmail.com>
2023-11-08 19:31:55 +0900

    Update documentation

--- dosaku/__init__.py
+++ dosaku/__init__.py
@@ -1,22 +1,2 @@
-from dosaku.core.exceptions import (ActionDoesNotExist,
-                                    CodingError,
-                                    ExecutorPermissionRequired,
-                                    InterpreterError,
-                                    ModuleForTaskNotFound,
-                                    NameAttributeNotFound,
-                                    OptionNotSupported,
-                                    ServicePermissionRequired,
-                                    UnsupportedActionType)
+"""Dosaku namespace."""
 from dosaku.config.config import Config
-from dosaku.core.actor import Actor
-from dosaku.core.task_connection import TaskConnection
-from dosaku.core.task_info import TaskInfo
-from dosaku.core.module_info import ModuleInfo
-from dosaku.core.task_hub import task_hub
-from dosaku.core.module_manager import module_manager
-from dosaku.core.task import Task
-from dosaku.core.module import Module
-from dosaku.core.service import Service
-from dosaku.core.executor import Executor
-from dosaku.core.stm import ShortTermModule
-from dosaku.core.agent import Agent
